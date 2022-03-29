56051263e8186cea0ca17def7aa26b3b92d29411
AleksandrZinoviev <aleks.rn.63@gmail.com>
2022-03-29 21:47:20 +0300

    E007

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <ul>
-        <li>A</li>
-        <li>B</li>
-        <li>C</li>
-        <li>D</li>
-      </ul>
+  <div>
+    <Header />
+    <Teschnologies />
+  </div>
+  )
+}
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <a href='#'>Home</a>
+      <a href='#'>News Feed</a>
+      <a href='#'>Messages</a>
     </div>
   )
 }
 
+const Teschnologies = () => {
+  return (
+    <div>
+      <ul>
+        <li><a href='#'>HTML</a></li>
+        <li><a href='#'>CSS</a></li>
+        <li><a href='#'>JavaScript</a></li>
+      </ul>
+    </div>
+  )
+}
 export default App;
