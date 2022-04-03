50abb159f0a434f2953691f657d71b3feac6d353
AleksandrZinoviev <aleks.rn.63@gmail.com>
2022-04-03 22:38:11 +0300

    012

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img className="logo" src="https://media.lpgenerator.ru/uploads/2019/07/11/1_thumb600x460.jpg" />
+        </header>
+    )
+}
+
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <main className="content">
+            <div>
+                <img src="https://img1.akspic.ru/previews/9/0/2/6/6/166209/166209-osen-sezon-korichnevyj_cvet-rastenie-ekoregion-550x310.jpg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+    </main>
+    );
+}
+
+export default Main;
