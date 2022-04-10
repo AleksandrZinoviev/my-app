56de58219700fe23f6058effc0a5e65a93265fae
AleksandrZinoviev <aleks.rn.63@gmail.com>
2022-04-11 00:46:19 +0300

    020

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Main from './components/Main/Main';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-const App = () => {
+const App = (props) => {
   return (
-  <div className="app-wrapper">
-    <Header />
-    <Navbar />
-    <div className="app-wrapper-content">
-      {/*<Main />*/}
-      <Dialogs />
-    </div>
-  </div>
-  )
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+          <div className="app-wrapper-content">
+          <Routes>
+                 <Route path="/dialogs" element= {<Dialogs/>}/>
+                 <Route path="/main" element={<Main/>}/>
+             </Routes>
+          </div>
+        
+      </div>
+      </BrowserRouter>)
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
 import React from "react"
+import { Link } from "react-router-dom";
 import styleSelect from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
     <nav className={styleSelect.nav}>
       <ul>
-        <li className={styleSelect.item}><a className={styleSelect.item_link} href="/main">Profile</a></li>
-        <li className={styleSelect.item}><a className={styleSelect.item_link} href="/dialogs">Messages</a></li>
-        <li className={styleSelect.item}><a className={styleSelect.item_link} href="#">News</a></li>
-        <li className={styleSelect.item}><a className={styleSelect.item_link} href="#">Music</a></li>
-        <li className={styleSelect.item}><a className={styleSelect.item_link} href="#">Settings</a></li>
+        <li className={styleSelect.item}>
+          <Link className={styleSelect.item_link} to="/main">Profile </Link>
+        </li>
+        <li className={styleSelect.item}>
+          <Link className={styleSelect.item_link} to="/dialogs">Messages</Link>
+        </li>
+        <li className={styleSelect.item}>
+          <a className={styleSelect.item_link} href="#">News</a>
+        </li>
+        <li className={styleSelect.item}>
+          <a className={styleSelect.item_link} href="#">Music</a>
+        </li>
+        <li className={styleSelect.item}>
+         <a className={styleSelect.item_link} href="#">Settings</a>
+        </li>
       </ul>
     </nav>
     );
 }
-    export default Navbar;
+    
+
+export default Navbar;
