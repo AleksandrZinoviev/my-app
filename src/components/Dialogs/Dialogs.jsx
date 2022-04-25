b588a26b142f196dbfd85c5e656929c761767113
AleksandrZinoviev <aleks.rn.63@gmail.com>
2022-04-25 23:34:01 +0300

    24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,7 @@ import styleSelect from "./Dialogs.module.css";
 
 const DialogItem = (props) => {
 
-    let path = "/dialogs/user_" + props.url;
+    let path = "/dialogs/user_" + props.id;
 
     return (
         <li className={styleSelect.dialogs_item}>
@@ -20,26 +20,50 @@ const Messageitem = (props) => {
     )
 }
 const Dialogs = (props) => {
+
+    let dialogsListArray = [
+
+        {id: 0, name: "user_0"},
+        {id: 1, name: "user_1"},
+        {id: 2, name: "user_2"},
+        {id: 3, name: "user_3"},
+        {id: 4, name: "user_4"},
+        {id: 5, name: "user_5"},
+        {id: 6, name: "user_6"}
+    ];
+
+    let messagesListArray = [
+
+        {id: 0, msg: "Hello"},
+        {id: 1, msg: "How are you"},
+        {id: 2, msg: "i`m fine and you"},
+        {id: 3, msg: "i`m very old and i tired"},
+        {id: 4, msg: "Hi, i`m User_4 is so cool"},
+        {id: 5, msg: "bored"},
+        {id: 6, msg: "I would like hm i dont no i would like"}
+    ];
+        
+
     return (
     <div className={styleSelect.ccontent}>
         <div className={styleSelect.dialogs}>
             <ul className={styleSelect.dialogs_list}>
-               <DialogItem name="user_0" url="0"/>
-               <DialogItem name="user_1" url="1"/>
-               <DialogItem name="user_3" url="2"/>
-               <DialogItem name="user_2" url="3"/>
-               <DialogItem name="user_4" url="4"/>
-               <DialogItem name="user_5" url="5"/>
-               <DialogItem name="user_6" url="6"/>
+               <DialogItem name={dialogsListArray[0].name} id={dialogsListArray[0].id}/>
+               <DialogItem name={dialogsListArray[1].name} id={dialogsListArray[1].id}/>
+               <DialogItem name={dialogsListArray[2].name} id={dialogsListArray[2].id}/>
+               <DialogItem name={dialogsListArray[3].name} id={dialogsListArray[3].id}/>
+               <DialogItem name={dialogsListArray[4].name} id={dialogsListArray[4].id}/>
+               <DialogItem name={dialogsListArray[5].name} id={dialogsListArray[5].id}/>
+               <DialogItem name={dialogsListArray[6].name} id={dialogsListArray[6].id}/>
             </ul>
             <ul className={styleSelect.messages_list}>
-                <Messageitem msg="Hello"/>
-                <Messageitem msg="How are you"/>
-                <Messageitem msg="i`m fine and you"/>
-                <Messageitem msg="i`m very old and i tired"/>
-                <Messageitem msg="Hi, i`m User_4 is so cool"/>
-                <Messageitem msg="bored"/>
-                <Messageitem msg="I would like hm i dont no i would like"/>
+                <Messageitem msg={messagesListArray[0].msg}/>
+                <Messageitem msg={messagesListArray[1].msg}/>
+                <Messageitem msg={messagesListArray[2].msg}/>
+                <Messageitem msg={messagesListArray[3].msg}/>
+                <Messageitem msg={messagesListArray[4].msg}/>
+                <Messageitem msg={messagesListArray[5].msg}/>
+                <Messageitem msg={messagesListArray[6].msg}/>
                 
             </ul>
         </div>
--- src/components/Main/MyPosts/MyPosts.jsx
+++ src/components/Main/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import styleSelect from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
+
+    let postArray = [
+        {id: 0, message: "Hi, how are you?", like: 20},
+        {id: 1, message: "It`s my first post.", like: 60}
+    ]
+
     return(
             <div className={styleSelect.myposts_wrapper}>
                 <h3>My posts</h3>
@@ -14,8 +20,8 @@ const MyPosts = () => {
                     <button>Remove</button>
                 </div>
                 <div className={styleSelect.posts}>
-                    <Post message="Hi, how are you?" like="20"/>
-                    <Post message="It`s my first post." like="60"/>
+                    <Post message={postArray[0].message} like={postArray[0].like}/>
+                    <Post message={postArray[1].message} like={postArray[1].like}/>
                 </div>
             </div>
     );
