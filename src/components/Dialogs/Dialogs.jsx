468aa36a4cbdbfaed9ff28ad71ef3778129e7b76
AleksandrZinoviev <aleks.rn.63@gmail.com>
2022-04-27 00:56:25 +0300

    25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -31,9 +31,11 @@ const Dialogs = (props) => {
         {id: 5, name: "user_5"},
         {id: 6, name: "user_6"}
     ];
-
+    let dialogsItem = dialogsListArray.map((el) => {
+       return <DialogItem name={el.name} id={el.id}/>
+    });
     let messagesListArray = [
-
+    
         {id: 0, msg: "Hello"},
         {id: 1, msg: "How are you"},
         {id: 2, msg: "i`m fine and you"},
@@ -42,29 +44,18 @@ const Dialogs = (props) => {
         {id: 5, msg: "bored"},
         {id: 6, msg: "I would like hm i dont no i would like"}
     ];
-        
 
+    let messagesItem = messagesListArray.map((el) => {
+        return <Messageitem msg={el.msg}/>
+     });
     return (
     <div className={styleSelect.ccontent}>
         <div className={styleSelect.dialogs}>
             <ul className={styleSelect.dialogs_list}>
-               <DialogItem name={dialogsListArray[0].name} id={dialogsListArray[0].id}/>
-               <DialogItem name={dialogsListArray[1].name} id={dialogsListArray[1].id}/>
-               <DialogItem name={dialogsListArray[2].name} id={dialogsListArray[2].id}/>
-               <DialogItem name={dialogsListArray[3].name} id={dialogsListArray[3].id}/>
-               <DialogItem name={dialogsListArray[4].name} id={dialogsListArray[4].id}/>
-               <DialogItem name={dialogsListArray[5].name} id={dialogsListArray[5].id}/>
-               <DialogItem name={dialogsListArray[6].name} id={dialogsListArray[6].id}/>
+                {dialogsItem}
             </ul>
             <ul className={styleSelect.messages_list}>
-                <Messageitem msg={messagesListArray[0].msg}/>
-                <Messageitem msg={messagesListArray[1].msg}/>
-                <Messageitem msg={messagesListArray[2].msg}/>
-                <Messageitem msg={messagesListArray[3].msg}/>
-                <Messageitem msg={messagesListArray[4].msg}/>
-                <Messageitem msg={messagesListArray[5].msg}/>
-                <Messageitem msg={messagesListArray[6].msg}/>
-                
+                {messagesItem}   
             </ul>
         </div>
     </div>)
